50d45843969d94e593d8bafe741eafd6d45f02c9
Xansiety <ferando.code@gmail.com>
2025-02-09 11:17:46 -0600

    Feat: 1st Day

---
+++ main.py
@@ -0,0 +1,18 @@
+# print("Enter a TODO:")
+# user_text = input() # variable - input read content of user entered
+# print(user_text)
+
+user_prompt = "Enter a TODO:"
+todo1 = input(user_prompt) # can make a placeholder with intput, and its able to receive
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+# print(text)
+
+# store multiple items
+todos = [todo1, todo2, todo3] #python list
+print(todos)
+
+# types
+print(type(user_prompt))
+print(type(todo1))
+print(type(todos))
