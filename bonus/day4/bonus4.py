7847d8f1df4f173f8b80628e01414e298906a831
Xansiety <ferando.code@gmail.com>
2025-02-09 13:46:34 -0600

    Feat(prev): Added Day 4

---
+++ bonus/day4/bonus4.py
@@ -0,0 +1,3 @@
+# Data in-mutability
+filenames = ["1. Raw Data.txt", "2.Reports.txt", "3.Presentation.txt"]
+
--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
     user_action = user_action.strip().lower()
 
     match user_action:
         case "add":
             todo = input("Enter a todo: ")
             todos.append(todo.capitalize())
+        case "edit":
+            number = int(input("Enter a number of the TODO to edit: ")) # all inputs are string, needs be parsed with int
+            number = number - 1
+            # -- list indexing --
+            existing_todo = todos[number]
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
         case "show" | "display":
             for item in todos:
                 item = item.title()
