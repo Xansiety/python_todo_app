68e9d8b94a6decb23e496d96a4f0a7626c0692f2
Xansiety <ferando.code@gmail.com>
2025-02-09 12:22:39 -0600

    Feat: Added day 2

---
+++ excersices/day2/ex2.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+
+x = 1
+while x <= 5:
+    print(name.capitalize())
+    x = x + 1
--- experiments/day2/e2.py
+++ experiments/day2/e2.py
@@ -1,10 +1,10 @@
 user_prompt = "Enter a TODO:"
-
 todos = []
 
 # While loop
 while True:
     todo = input(user_prompt)
     todo.capitalize() # Capitalize first letter
+    print(todo.title()) # Capitalize every first letter from words
     todos.append(todo)
     print(todos)
