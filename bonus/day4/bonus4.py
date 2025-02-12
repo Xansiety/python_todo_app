81c7627ee6caac4ffa689e4dbc17c91888cbbb6f
Xansiety <ferando.code@gmail.com>
2025-02-11 22:28:13 -0600

    Feat: added day 5

--- bonus/day4/bonus4.py
+++ bonus/day4/bonus4.py
@@ -1,3 +1,25 @@
 # Data in-mutability
-filenames = ["1. Raw Data.txt", "2.Reports.txt", "3.Presentation.txt"]
+filenames = ["1.Raw Data.txt", "2.Reports.txt", "3.Presentation.txt"]
 
+for filename in filenames:
+    filename = filename.replace('.', '-', 1) # only replace the first ocurrence
+    print(filename)
+
+
+# List can be inmutables using tuples - use () instead []
+filename = ("1.Raw Data.txt", "2.Reports.txt", "3.Presentation.txt")
+
+# filename
+# ('1.Raw Data.txt', '2.Reports.txt', '3.Presentation.txt')
+# type(filename)
+# <class 'tuple'>
+# filename[1]
+# '2.Reports.txt'
+# filename[1] = "something new"
+# Traceback (most recent call last):
+#   File "<input>", line 1, in <module>
+# TypeError: 'tuple' object does not support item assignment
+# filename.append()
+# Traceback (most recent call last):
+#   File "<input>", line 1, in <module>
+# AttributeError: 'tuple' object has no attribute 'append'
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit: ")
+    user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip().lower()
 
     match user_action:
@@ -15,10 +15,13 @@ while True:
             existing_todo = todos[number]
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
+        case "complete":
+            number = int(input("Enter a number to complete: "))
+            todos.pop(number - 1)
         case "show" | "display":
-            for item in todos:
-                item = item.title()
-                print(item)
+            for index, item in enumerate(todos):
+                row = f"{index + 1}. {item}" # interpolation data
+                print(row)
         case "exit":
             break
         case _:
